0e99b5f0797da50709fad1bec45eb9874119c821
julian-saleix <julian.saleix@gmail.com>
2025-07-18 15:50:16 +0200

    chore: removing useless dependencies in worker

--- apps/worker/out/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const db_1 = require("./core/db");
-const workers_1 = require("./queues/workers");
-workers_1.addMovieWorker.run();
-workers_1.ratingWorker.run();
-workers_1.summaryWorker.run();
-db_1.db.$disconnect();
