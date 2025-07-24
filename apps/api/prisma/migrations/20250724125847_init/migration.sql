94b23cc23c5991dde09b1fdb09fdaf83da2db92f
julian-saleix <julian.saleix@gmail.com>
2025-07-24 14:59:58 +0200

    chore: adding User in prisma schema

--- apps/api/prisma/migrations/20250724125847_init/migration.sql
+++ apps/api/prisma/migrations/20250724125847_init/migration.sql
@@ -4,11 +4,13 @@ CREATE TYPE "RatingUnit" AS ENUM ('points', 'stars', 'percentage');
 -- CreateEnum
 CREATE TYPE "RatingsStatus" AS ENUM ('pending', 'completed', 'failed');
 
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('user', 'admin', 'mod');
+
 -- CreateTable
 CREATE TABLE "Movie" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
-    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "tmdbId" INTEGER NOT NULL,
     "tagLine" TEXT NOT NULL,
     "summary" TEXT NOT NULL,
@@ -17,6 +19,8 @@ CREATE TABLE "Movie" (
     "year" INTEGER NOT NULL,
     "budget" DOUBLE PRECISION NOT NULL,
     "poster_path" TEXT NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
     "ratings_status" "RatingsStatus" NOT NULL DEFAULT 'pending',
 
     CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
@@ -41,14 +45,35 @@ CREATE TABLE "Movie_Request" (
     "id" TEXT NOT NULL,
     "tmdbId" INTEGER NOT NULL,
     "title" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "processed" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "Movie_Request_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'user',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Movie_tmdbId_key" ON "Movie"("tmdbId");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
 -- AddForeignKey
 ALTER TABLE "Movie_Rating" ADD CONSTRAINT "Movie_Rating_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "Movie"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Movie_Request" ADD CONSTRAINT "Movie_Request_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
