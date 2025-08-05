9daaae61f235ece82f9dc093be2487c9038b86f9
julian-saleix <julian.saleix@gmail.com>
2025-08-05 18:25:21 +0200

    chore: client not found page using pageHeader

--- apps/client/src/features/general/pages/not-found/index.tsx
+++ apps/client/src/features/general/pages/not-found/index.tsx
@@ -1,3 +1,15 @@
+import PageHeader from "../../../../shared/ui/page-header";
+
 export default function NotFoundPage() {
-    return <p>The page you're looking for does not exist</p>;
+    return (
+        <div className="w-full">
+            <div className="flex flex-col container mx-auto gap-5 py-5 px-5 md:px-0">
+                <PageHeader title="Oops 😥">
+                    <p className="text-text-secondary">
+                        The page you're looking for does not exist
+                    </p>
+                </PageHeader>
+            </div>
+        </div>
+    );
 }
