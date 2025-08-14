1efd39506f01db525a9662d5e786cb02aa7fc2b1
julian-saleix <julian.saleix@gmail.com>
2025-08-14 22:56:37 +0200

    fix: client build

--- apps/client/src/assets/data-test/user.ts
+++ apps/client/src/assets/data-test/user.ts
@@ -4,4 +4,6 @@ export const mockUserProfile = {
     id: "1",
     email: "john.doe@gmail.com",
     role: "user",
+    username: "JohnDo",
+    created_at: new Date().toDateString(),
 } satisfies UserType;
--- apps/client/src/shared/api/headers.ts
+++ apps/client/src/shared/api/headers.ts
@@ -1,5 +1,3 @@
-import { STORAGE_TOKEN_KEY } from "../../core/config/storage";
-
 export const authHeaders = () => {
     const headers: Record<string, string> = {};
     // const token = localStorage.getItem(STORAGE_TOKEN_KEY);
