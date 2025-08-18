cdd376f169a8a18015f9530d64e61a872cab96b6
julian-saleix <julian.saleix@gmail.com>
2025-08-18 22:35:33 +0200

    debug: possible fix for worker on coolify

--- apps/worker/src/cli/index.ts
+++ apps/worker/src/cli/index.ts
@@ -9,7 +9,7 @@ async function main() {
         const [name, year] = args;
         if (!name) throw new Error("No name provided");
         if (!year) throw new Error("No year provided");
-        console.log(`Gathering Rotten Tomatoes scores for ${name} ${year}`);
+        console.log(`Gathering Rotten Tomatoes scores for ${name} - ${year}`);
         console.log(await getRottenTomatoesScores(name, +year));
     } catch (e) {
         console.error(e);
