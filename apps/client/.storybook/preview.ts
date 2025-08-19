ef4f101d58a72b662b348da19f3b0f391270b9c1
julian-saleix <julian.saleix@gmail.com>
2025-08-19 18:26:44 +0200

    chore: adding lastMoviesAdded Story + fix MoviePosterItem width

--- apps/client/.storybook/preview.ts
+++ apps/client/.storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from "@storybook/react-vite";
 import "../src/index.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const preview: Preview = {
     parameters: {
--- apps/client/src/features/movies/components/movie-poster-item.tsx
+++ apps/client/src/features/movies/components/movie-poster-item.tsx
@@ -16,7 +16,7 @@ export default function MoviePosterItem({ movie }: Props) {
     return (
         <Link
             to={`/movies/${movie.id}`}
-            className="hover:opacity-90 duration-150 rounded-md overflow-hidden w-fit"
+            className="block hover:opacity-90 duration-150 rounded-md overflow-hidden w-55"
         >
             <div className="w-55 overflow-hidden object-contain shadow-xl">
                 <img
