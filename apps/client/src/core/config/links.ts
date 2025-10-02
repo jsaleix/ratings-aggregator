841296a0812f94e8ecea1a9ca1d9ec5c3b030967
julian-saleix <julian.saleix@gmail.com>
2025-10-02 15:42:31 +0200

    chore: adding Tos link in signup

---
+++ apps/client/src/core/config/links.ts
@@ -0,0 +1,6 @@
+export const LEGALS_LINKS = {
+    termsOfUse:
+        "https://docs.google.com/document/d/1kPtMDzq4b4Btc9es2nGkVfmJWuOm6wMzsLvuBovujPY/edit?usp=sharing",
+    privacy:
+        "https://docs.google.com/document/d/1U9K4JJt03XlQole3SftyZnVFOMwt5yKjd-hspuFoGTU/edit?usp=sharing",
+};
--- apps/client/src/features/auth/components/signup-form.stories.tsx
+++ apps/client/src/features/auth/components/signup-form.stories.tsx
@@ -1,9 +1,17 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Component from "./signup-form";
+import { MemoryRouter } from "react-router";
 
 const meta: Meta<typeof Component> = {
     title: "Auth/SignupForm",
     component: Component,
+    decorators: [
+        (Story) => (
+            <MemoryRouter>
+                <Story />
+            </MemoryRouter>
+        ),
+    ],
 };
 
 export default meta;
