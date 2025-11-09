6e4c66276cdff64b5accd62d4c51a32321e7ac09
julian-saleix <julian.saleix@gmail.com>
2025-11-09 20:08:25 +0100

    chore: adding HeroRating item for home page

---
+++ apps/client/src/features/general/components/hero-ratings/index.tsx
@@ -0,0 +1,138 @@
+import { motion, useMotionValue, useSpring, useTransform } from "motion/react";
+import { useEffect, useRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+import MovieRatingItem from "../../../movies/components/movie-rating-item";
+
+const MOCK_RATINGS = [
+    {
+        id: "16889e8b-3c6e-4ff4-88ef-f247eca8ede7",
+        movieId: "79c9f9f3-e2a3-415c-87dc-3856f05ac375",
+        rating_source: "allocine_audience",
+        value: "3,8",
+        rating_unit: "stars",
+        extra: "",
+        sourceUrl: null,
+        created_at: "2025-11-07T16:26:50.230Z",
+        updated_at: "2025-11-07T16:26:50.230Z",
+    },
+    {
+        id: "c98d3776-9a02-44e9-a07a-5fbbd8c57e62",
+        movieId: "79c9f9f3-e2a3-415c-87dc-3856f05ac375",
+        rating_source: "allocine_press",
+        value: "5,0",
+        rating_unit: "stars",
+        extra: "",
+        sourceUrl: null,
+        created_at: "2025-11-07T16:26:50.216Z",
+        updated_at: "2025-11-07T16:26:50.216Z",
+    },
+    {
+        id: "6af5c340-c26f-43eb-bd46-f38824e97ef9",
+        movieId: "79c9f9f3-e2a3-415c-87dc-3856f05ac375",
+        rating_source: "imdb",
+        value: "7.7",
+        rating_unit: "points",
+        extra: "",
+        sourceUrl: "",
+        created_at: "2025-11-07T16:26:53.000Z",
+        updated_at: "2025-11-07T16:26:53.000Z",
+    },
+    {
+        id: "0245ba64-2db4-4960-9c0c-95a932631ffe",
+        movieId: "79c9f9f3-e2a3-415c-87dc-3856f05ac375",
+        rating_source: "letterboxd",
+        value: "3.9",
+        rating_unit: "stars",
+        extra: "",
+        sourceUrl: "",
+        created_at: "2025-11-07T16:26:54.189Z",
+        updated_at: "2025-11-07T16:26:54.189Z",
+    },
+    {
+        id: "b0d3f72a-0dc9-4708-b5fd-1878260b86e1",
+        movieId: "79c9f9f3-e2a3-415c-87dc-3856f05ac375",
+        rating_source: "rotten_tomatoes",
+        value: "85%",
+        rating_unit: "percentage",
+        extra: "",
+        sourceUrl: "",
+        created_at: "2025-11-07T16:26:55.616Z",
+        updated_at: "2025-11-07T16:26:55.616Z",
+    },
+    {
+        id: "04622809-adcf-42f8-81a5-ad67fc6ecee2",
+        movieId: "79c9f9f3-e2a3-415c-87dc-3856f05ac375",
+        rating_source: "rotten_tomatoes_audience",
+        value: "95%",
+        rating_unit: "percentage",
+        extra: "",
+        sourceUrl: "",
+        created_at: "2025-11-07T16:26:55.622Z",
+        updated_at: "2025-11-07T16:26:55.622Z",
+    },
+];
+
+interface Props {
+    className?: string;
+}
+export default function HeroRatings({ className }: Props) {
+    const style = twMerge(
+        "relative bg-black/50 backdrop-blur-sm rounded-xl w-fit h-fit py-5 px-8",
+        className
+    );
+    const ref = useRef<HTMLDivElement>(null);
+
+    const x = useMotionValue(0);
+    const y = useMotionValue(0);
+
+    const springX = useSpring(x, { stiffness: 50, damping: 18 });
+    const springY = useSpring(y, { stiffness: 50, damping: 18 });
+
+    const rotateX = useTransform(springY, [0, 400], [20, -20]);
+    const rotateY = useTransform(springX, [0, 400], [-20, 20]);
+
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        x.set(e.clientX - rect.left);
+        y.set(e.clientY - rect.top);
+    };
+
+    const handleMouseLeave = () => {
+        const rect = ref.current?.getBoundingClientRect();
+        if (!rect) return;
+        x.set(rect.width / 2);
+        y.set(rect.height / 2);
+    };
+
+    useEffect(() => {
+        if (ref.current) {
+            const rect = ref.current.getBoundingClientRect();
+            x.set(rect.width / 2);
+            y.set(rect.height / 2);
+        }
+    }, []);
+
+    return (
+        <motion.div
+            ref={ref}
+            className={style}
+            onMouseMove={handleMouseMove}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 0.4 } }}
+            style={{
+                rotateX,
+                rotateY,
+                transformStyle: "preserve-3d",
+                perspective: 600,
+            }}
+            onMouseLeave={handleMouseLeave}
+        >
+            <motion.div className="inset-5 w-96 h-fit">
+                {MOCK_RATINGS.map((rating) => (
+                    <MovieRatingItem rating={rating} key={rating.id} />
+                ))}
+            </motion.div>
+        </motion.div>
+    );
+}
--- apps/client/src/features/general/pages/home.tsx
+++ apps/client/src/features/general/pages/home.tsx
@@ -5,6 +5,7 @@ import { useAuthContext } from "../../../core/auth/provider";
 import LastMoviesAdded from "../../movies/components/movie-posters-section/last-movies-added";
 import LastMoviesUpdated from "../../movies/components/movie-posters-section/last-movies-updated";
 import RandomMovies from "../../movies/components/movie-posters-section/random-movies";
+import HeroRatings from "../components/hero-ratings";
 
 export default function HomePage() {
     const { isConnected } = useAuthContext();
@@ -13,14 +14,17 @@ export default function HomePage() {
         <div className="w-full">
             <div className="flex flex-col gap-0">
                 <section className="w-full flex md:flex-row items-center gap-5 justify-center bg-black h-[80vh] md:h-[50vh] z-1 overflow-hidden relative">
-                    <div className="container mx-auto flex flex-row items-center w-full relative h-full">
-                        <div className="flex flex-col w-fit z-1 items-center md:items-start gap-5 p-5 md:p-0">
+                    <div className="z-1 container mx-auto flex flex-row gap-10 items-center w-full relative h-full">
+                        <div className="flex flex-col w-fit z-1 items-center md:items-start gap-5 p-5 lg:p-0">
                             <h2 className="uppercase text-white text-4xl font-bold text-center md:text-start text-shadow-lg">
-                                Your all-in-one<br/>hub for movie ratings
+                                Your all-in-one
+                                <br />
+                                hub for movie ratings
                             </h2>
                             <p className="text-white text-2xl md:text-xl text-shadow-lg font-light md:text-start text-center">
-                                See how films score across<br/>IMDb, Metacritic, and
-                                more — instantly.
+                                See how films score across
+                                <br />
+                                IMDb, Metacritic, and more — instantly.
                             </p>
                             {!isConnected && (
                                 <Link to={"/auth"} className="">
@@ -30,6 +34,7 @@ export default function HomePage() {
                                 </Link>
                             )}
                         </div>
+                        <HeroRatings className="hidden md:block md:scale-65 lg:scale-75 xl:scale-100" />
                     </div>
                     <div
                         className="
