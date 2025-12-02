cc5f083b47c5b350d59b569cab4091151edd86c9
julian-saleix <julian.saleix@gmail.com>
2025-12-02 19:03:26 +0100

    fix: api works locally & in docker

--- apps/api/src/movies/dto/create-movie.dto.ts
+++ apps/api/src/movies/dto/create-movie.dto.ts
@@ -1,5 +1,5 @@
 import { Transform } from 'class-transformer';
-import { IsDate, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateMovieDto {
   @IsString()
