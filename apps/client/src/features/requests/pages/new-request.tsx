922ef5fbd4a3320daaa40ccf8ab10caebd607d84
julian-saleix <julian.saleix@gmail.com>
2025-12-08 18:26:36 +0100

    chore: newRequest allows searchByTitle

--- apps/client/src/features/requests/pages/new-request.tsx
+++ apps/client/src/features/requests/pages/new-request.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router";
 import { displayMsg } from "../../../shared/utils/toast";
 import PageHeader from "../../../shared/ui/page-header";
 import Button from "../../../shared/ui/button";
-import { useAuthContext } from "../../../core/auth/provider";
+
 import type { CreateRequestType } from "../types/schemas";
 import apiRequestService from "../services/api-request.service";
 import RequestForm from "../components/new-request-form";
@@ -13,10 +13,7 @@ import RequestPremiumForm from "../components/new-request-premium-form";
 
 export default function NewRequestPage() {
     const navigate = useNavigate();
-    const { role } = useAuthContext();
     const [searchBy, setSearchBy] = useState<"title" | "tmdbId">("tmdbId");
-
-    const displayChoices = role === "admin" || role === "premium";
 
     const { mutateAsync: createRequest } = useMutation({
         mutationFn: async (request: CreateRequestType) => {
@@ -35,28 +32,24 @@ export default function NewRequestPage() {
         <div className="w-full">
             <div className="flex flex-col container mx-auto gap-5 py-5 px-5 md:px-0">
                 <PageHeader title="New request">
-                    {displayChoices && (
-                        <div className="flex items-center gap-5">
-                            <Button
-                                variant={
-                                    searchBy === "title" ? "primary" : "default"
-                                }
-                                onClick={() => setSearchBy("title")}
-                            >
-                                Search by Title
-                            </Button>
-                            <Button
-                                variant={
-                                    searchBy === "tmdbId"
-                                        ? "primary"
-                                        : "default"
-                                }
-                                onClick={() => setSearchBy("tmdbId")}
-                            >
-                                Search by TMDB ID
-                            </Button>
-                        </div>
-                    )}
+                    <div className="flex items-center gap-5">
+                        <Button
+                            variant={
+                                searchBy === "title" ? "primary" : "default"
+                            }
+                            onClick={() => setSearchBy("title")}
+                        >
+                            Search by Title
+                        </Button>
+                        <Button
+                            variant={
+                                searchBy === "tmdbId" ? "primary" : "default"
+                            }
+                            onClick={() => setSearchBy("tmdbId")}
+                        >
+                            Search by TMDB ID
+                        </Button>
+                    </div>
                 </PageHeader>
                 <div className="flex md:w-2/3">
                     {searchBy === "title" && (
