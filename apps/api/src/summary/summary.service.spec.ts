73b3a7b8a745a5df0e4c2b30f75e4849eb88bedb
julian-saleix <julian.saleix@gmail.com>
2025-12-23 19:31:39 +0100

    fix: dependencies error in api tests

--- apps/api/src/summary/summary.service.spec.ts
+++ apps/api/src/summary/summary.service.spec.ts
@@ -1,13 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SummaryService } from './summary.service';
 import { PrismaService } from 'src/shared/services/prisma.service';
+import { BullmqService } from 'src/shared/services/bullmq.service';
 
 describe('SummaryService', () => {
   let service: SummaryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SummaryService, PrismaService],
+      providers: [
+        SummaryService,
+        PrismaService,
+        {
+          provide: BullmqService,
+          useValue: jest.fn(),
+        },
+      ],
     }).compile();
 
     service = module.get<SummaryService>(SummaryService);
