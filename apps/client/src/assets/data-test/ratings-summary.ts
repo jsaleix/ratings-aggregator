ab0052e3acf977631ae76151fd85d9b18643bc78
julian-saleix <julian.saleix@gmail.com>
2026-02-12 18:26:10 +0100

    feat: client displays scoreValue percent score

--- apps/client/src/assets/data-test/ratings-summary.ts
+++ apps/client/src/assets/data-test/ratings-summary.ts
@@ -6,6 +6,7 @@ export const RatingsSummaryMockData = {
         "This film shows a transatlantic divide: American audiences (Rotten Tomatoes 81%, IMDb 6.6) appreciate it more than French viewers (Allociné 2.8 stars). Critics are lukewarm on both sides. (191 chars)",
     movieId: "585a1d03-373f-4624-8e5c-cb33d58d0805",
     score: "A",
+    score_value: 97,
     created_at: "2025-08-25T17:51:30.509Z",
     updated_at: "2025-08-25T17:51:30.509Z",
 } satisfies RatingsSummaryModel;
