b018aac058e6fb13ffe968a8726c42a8084afb04
julian-saleix <julian.saleix@gmail.com>
2026-02-27 22:40:37 +0100

    chore: (client) quick styling of pagination component

--- apps/client/src/shared/ui/pagination/index.tsx
+++ apps/client/src/shared/ui/pagination/index.tsx
@@ -8,6 +8,8 @@ interface Props {
 
 export default function Pagination({ data, onPageChange }: Props) {
     const lastPageIdx = Math.floor(data.total / data.perPage);
+    const style =
+        "bg-white hover:bg-white/80 duration-150 rounded-xs disabled:bg-gray-600 px-2 py-1 text-black cursor-pointer disabled:cursor-not-allowed";
 
     const handleOnPageChange = useCallback(
         (page: number) => {
@@ -17,30 +19,42 @@ export default function Pagination({ data, onPageChange }: Props) {
     );
 
     return (
-        <div data-testid="pagination">
+        <div data-testid="pagination" className="flex flex-row gap-3">
             <button
+                className={style}
                 data-testid="first"
                 disabled={data.currentPage === 1}
                 onClick={() => handleOnPageChange(1)}
-            ></button>
+            >
+                {"<<"}
+            </button>
             <button
+                className={style}
                 data-testid="previous"
                 data-pageid={data.prev}
                 disabled={data.currentPage <= 1}
                 onClick={() => data.prev && handleOnPageChange(data.prev)}
-            ></button>
+            >
+                {"<"}
+            </button>
             <button
+                className={style}
                 data-testid="next"
                 data-pageid={data.next}
                 disabled={!data.next}
                 onClick={() => data.next && handleOnPageChange(data.next)}
-            ></button>
+            >
+                {">"}
+            </button>
             <button
+                className={style}
                 data-testid="last"
                 data-pageid={lastPageIdx}
                 disabled={!data.next}
                 onClick={() => handleOnPageChange(lastPageIdx)}
-            ></button>
+            >
+                {">>"}
+            </button>
         </div>
     );
 }
