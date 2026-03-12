4b3f0aea8710d41375d3032ea885a2ad9ed5e3e1
julian-saleix <julian.saleix@gmail.com>
2026-03-12 17:19:10 +0100

    chore: (worker) removing setFetching on jobStatus + executing request update when movie queue complete

--- apps/worker/src/queues/movie/index.ts
+++ apps/worker/src/queues/movie/index.ts
@@ -4,6 +4,7 @@ import { QUEUES, RedisMqConnection } from "../../config/bullmq";
 import { logger } from "../../shared/logger";
 import { PrismaMovieJobPipelineService } from "../../shared/modules/movie-job-pipeline/services/prisma.service";
 import { MOVIE_STATUS } from "../../shared/modules/movie-job-pipeline/constants";
+import PrismaMovieRequestRepository from "../../shared/modules/movie-requests/repositories/prisma-request.repository";
 
 import PrismaMovieRepository from "../../features/movies/repositories/prisma-movie.repository";
 import TMDBService from "../../features/movies/services/tmdb.service";
@@ -12,7 +13,6 @@ import { AddMovieByTMDBIdUseCase } from "../../features/movies/use-cases/add-mov
 import { PrismaGenreRepository } from "../../features/movies/repositories/prisma-genre.repository";
 import { ratingQueue } from "..";
 import MovieHandler, { MovieJob } from "./handler";
-import PrismaMovieRequestRepository from "../../shared/modules/movie-requests/repositories/prisma-request.repository";
 
 const movieJobPipelineService = new PrismaMovieJobPipelineService();
 const tmdbService = new TMDBService();
@@ -38,39 +38,34 @@ export const movieWorker = new Worker(
 );
 
 movieWorker.on("active", async (job: Job<MovieJob>) => {
-    const { payload } = job.data;
     logger.info("Movie worker active", {
         tags: ["movie-worker", "worker"],
         payload: job.data.payload,
     });
-    try {
-        await movieRequestRepository.updateRequestState(
-            payload.requestId,
-            true,
-        );
-        await movieJobPipelineService.setFetching(payload.tmdbId);
-    } catch (error) {
-        logger.warn("Failed to update request state or movieJobPipeline", {
-            tags: ["movie-worker", "worker"],
-            payload,
-            error: error instanceof Error ? error.message : String(error),
-        });
-    }
 });
 
 movieWorker.on(
     "completed",
     async (job: Job<MovieJob>, movie: MovieType | undefined) => {
         if (movie == undefined) return;
-        const request_id = job.data.payload.requestId;
-        const tmdb_id = job.data.payload.tmdbId;
-        const movie_id = movie.id;
-        const movie_slug = movie.slug;
+        const { requestId: request_id, tmdbId: tmdb_id } = job.data.payload;
+        const { id: movie_id, slug: movie_slug } = movie;
 
         logger.info("Movie worker completed", {
             tags: ["movie-worker", "worker"],
             payload: { request_id, tmdb_id, movie_id, movie_slug },
         });
+        try {
+            await movieRequestRepository.updateRequestState(request_id, true);
+        } catch (e) {
+            logger.warn(
+                "Could not update request state, may have been deleted",
+                {
+                    tags: ["movie-worker", "worker"],
+                    payload: { request_id, tmdb_id, movie_id, movie_slug },
+                },
+            );
+        }
         await movieJobPipelineService.setRating(tmdb_id);
         await ratingQueue.add("set-ratings", {
             type: "movie",
